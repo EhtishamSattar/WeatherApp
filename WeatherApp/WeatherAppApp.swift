d6b2c86b056c8e9e51327967bf7bf93b4923c16f
EhtishamSattar <ehtishamsattar888@gmail.com>
2024-08-19 12:15:48 +0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mac on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
